d741602ec5e5cedf6aa11a3a9520cff544555bda
Leibriel <101602384+Leibriel@users.noreply.github.com>
2022-06-20 20:50:35 -0300

    Add files via upload

---
+++ TrabalhoFinal/src/Trabalho/Demo.java
@@ -0,0 +1,23 @@
+package Trabalho;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		// Demonstra System.out.write(). class WriteDemo {
+		
+			int b;
+			b = 'A';
+			System.out.write(b);
+			System.out.write('\n');
+			int x='C';
+			System.out.write(x);
+			System.out.write('\n');
+			
+			
+	
+	}
+}
